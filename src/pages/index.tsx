0abb31133e89bf16d0be56c3fb5d7a86fc7ad285
Дмитрий Солодов <dmitrijsolodov@MacBook-Pro-Dmitrij.local>
2023-04-18 22:09:00 +0300

    'admin' delete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,6 @@
 import Layout from "@/components/layout/Layout";
-import {Button} from "@/components/UI/Button/Button";
-import styles from "../styles/home.module.scss"
-import Link from "next/link";
 
 export default function HomePage() {
     return <Layout title={'Страница шаблон'}>
-        <div className={styles.main}>
-            <Link href={'/admin'}>
-                <Button type={'button'}>
-                    Перейти в админку
-                </Button>
-            </Link>
-        </div>
     </Layout>
 }
