a90c23413a4c74ecfbf3c7d3be53e51ee76fe87d
Дмитрий Солодов <dmitrijsolodov@MacBook-Pro-Dmitrij.local>
2023-04-22 17:54:01 +0300

    correct api

--- src/pages/api/sendAnswer.ts
+++ src/pages/api/sendAnswer.ts
@@ -1,10 +1,10 @@
 import {NextApiRequest, NextApiResponse} from "next";
 import {PrismaClient} from "@prisma/client";
-import {Users} from "@/models/models";
+import {Subscriber} from "@/models/models";
 
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse<Users>,
+    res: NextApiResponse<Subscriber | { error: string }>,
 ) {
     const prisma = new PrismaClient()
     try {
@@ -22,10 +22,22 @@ export default async function handler(
                     ...req.body,
                     event_id: 1,
                 }
+            }).catch(() => {
+                throw Error()
             })
+            answer.then((data) => res.status(200).json({
+                name: data.name,
+                id: data.id,
+                alcohol: data.alcohol,
+                arrivement: data.arrivement
+            }));
+        } else {
+            res.status(405).json({error: 'Method Not Allowed'})
         }
+
     } catch (error) {
         console.error(error);
+        res.status(500).json({error: 'Internal server error'});
     } finally {
         await prisma.$disconnect();
     }
